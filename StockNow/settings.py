5f6cb6f95cdb893e162213982495658ae94c5a8f
Krishan-Patel <61567619+Krishan-Patel@users.noreply.github.com>
2021-05-06 21:25:22 -0400

    updated allowed hosts in settings.py

--- StockNow/settings.py
+++ StockNow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@k_h+0qcd#$pf$zemykd*o9(y(je6l
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['frozen-waters-61791.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
